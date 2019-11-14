93fd0eda7fc57da5b1f773d768947ab0452d985e
Felipe Christian Lautenschlager <1947866-felipelauten@users.noreply.gitlab.com>
2019-11-14 15:20:00 +0100

    Added new fragment for welcoming the user

--- app/src/main/java/br/com/devlauten/grabthis/MainActivity.java
+++ app/src/main/java/br/com/devlauten/grabthis/MainActivity.java
@@ -1,14 +1,26 @@
 package br.com.devlauten.grabthis;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.FirebaseApp;
+
+public class MainActivity extends AppCompatActivity
+        implements WelcomeFragment.OnFragmentInteractionListener {
+
+    FirebaseApp app = FirebaseApp.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        //TODO: Check what to do here!
     }
 }
